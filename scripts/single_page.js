284a5b11770bf9534dc7a5f6bcdc45ebdd1b913e
Pete <peternatewood@gmail.com>
2016-02-08 17:16:46 -0500

    Update single_page js to replace extra html tags from response

--- scripts/single_page.js
+++ scripts/single_page.js
@@ -1,9 +1,14 @@
+var getMain = function(htmlString) {
+	return htmlString.replace(/(.*\s*){3}/, "").replace("</body>", "").replace("</html>", "").trim();
+}
+
 $(document).ready(function() {
 	$('a').on('click', function(event) {
 		event.preventDefault();
 		$.ajax(event.target.href)
 		.done(function(response) {
-			console.log(event.target, response);
+			window.htmlString = response;
+			console.log("regex'd!", getMain(htmlString));
 		})
 		.fail(function(response) {
 			console.log('failure?', response);
