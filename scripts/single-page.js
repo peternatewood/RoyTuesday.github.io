ea53c620bb5ff7acfee472dfcc7aa1064d5ac874
Pete <peternatewood@gmail.com>
2016-02-13 11:59:47 -0500

    Add ajax get request for every link

--- scripts/single-page.js
+++ scripts/single-page.js
@@ -3,6 +3,17 @@ $(document).on('ready', function() {
 
   $('a').on('click', function(event) {
     event.preventDefault();
-    console.log('links have been bound!');
+    console.log('links have been bound!', event.target);
+    var targetURI = event.target.href;
+    console.log('targetURI', targetURI)
+    $.ajax({
+      cache: false,
+      url: targetURI
+    }).done(function(response) {
+      console.log('success!', response);
+    }).fail(function(response) {
+      console.log('failure?', response);
+    });
   });
+
 });
