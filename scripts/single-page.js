d9bb6f9c366745af765c3fd0c00d328a63dac7b8
Pete <peternatewood@gmail.com>
2016-02-13 11:42:58 -0500

    Add event bindings to links

--- scripts/single-page.js
+++ scripts/single-page.js
@@ -1,3 +1,8 @@
 $(document).on('ready', function() {
   console.log("sanity check okay!");
+
+  $('a').on('click', function(event) {
+    event.preventDefault();
+    console.log('links have been bound!');
+  });
 });
