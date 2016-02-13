5f6fad16ad94b01ee9770f8ac10ed1336068736d
Pete <peternatewood@gmail.com>
2016-02-13 14:23:54 -0500

    Remove console logs from blog link binding

--- scripts/single-page.js
+++ scripts/single-page.js
@@ -14,14 +14,11 @@ $(document).on('ready', function() {
   $('a.blog-link').on('click', function(event) {
     event.preventDefault();
     var targetURI = event.target.dataset.pagePartial;
-    console.log("I'm a blog link!", targetURI);
     $.ajax({
       cache: false,
       url: targetURI
     }).done(function(response) {
-      console.log('success!', response);
-    }).fail(function(response) {
-      console.log('failure?', response);
+      $('main').html(response);
     });
   });
 
