ef1eb80604ffaebad3e0d9bf304d32f2dbc59c60
Pete <peternatewood@gmail.com>
2016-02-13 12:09:55 -0500

    Update single-page js to get ajax uri from pagePartial attribute

--- scripts/single-page.js
+++ scripts/single-page.js
@@ -4,7 +4,7 @@ $(document).on('ready', function() {
   $('a').on('click', function(event) {
     event.preventDefault();
     console.log('links have been bound!', event.target);
-    var targetURI = event.target['data-json-target'];
+    var targetURI = event.target.dataset.pagePartial;
     console.log('targetURI', targetURI)
     $.ajax({
       cache: false,
