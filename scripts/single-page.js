3628490db33e29ceceac6787a1c2ecd122916a66
Pete <peternatewood@gmail.com>
2016-02-13 14:04:05 -0500

    Remove console logs from single-page js

--- scripts/single-page.js
+++ scripts/single-page.js
@@ -1,19 +1,13 @@
 $(document).on('ready', function() {
-  console.log("sanity check okay!");
 
   $('nav a').on('click', function(event) {
     event.preventDefault();
-    console.log('links have been bound!', event.target);
     var targetURI = event.target.dataset.pagePartial;
-    console.log('targetURI', targetURI)
     $.ajax({
       cache: false,
       url: targetURI
     }).done(function(response) {
-      console.log('success!', response);
       $('main').html(response);
-    }).fail(function(response) {
-      console.log('failure?', response);
     });
   });
 
