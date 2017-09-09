ede8c24ad5bc89be4fd9a05ff9f467792818286c
Pete <peternatewood@gmail.com>
2017-09-09 19:26:35 -0400

    Play tones when clicking buttons in webaudio basics blog

--- scripts/webaudio-basics.js
+++ scripts/webaudio-basics.js
@@ -1,4 +1,75 @@
 ready(function() {
+  var audio;
+  if (AudioContext) {
+    audio = new AudioContext;
+  }
+  else if (webkitAudioContext) {
+    audio = new webkitAudioContext;
+  }
+
+  function playRawOsc(e) {
+    e.preventDefault();
+
+    if (audio) {
+      var osc = audio.createOscillator();
+      osc.connect(audio.destination);
+      osc.start(audio.currentTime);
+      osc.stop(audio.currentTime + 0.1);
+    }
+  }
+  function playSawtooth(e) {
+    e.preventDefault();
+
+    if (audio) {
+      var osc = audio.createOscillator();
+      osc.type = 'sawtooth';
+      osc.frequency.setValueAtTime(220, audio.currentTime);
+      osc.connect(audio.destination);
+      osc.start(audio.currentTime);
+      osc.stop(audio.currentTime + 0.1);
+    }
+  }
+  function playSofter(e) {
+    e.preventDefault();
+
+    if (audio) {
+      var osc = audio.createOscillator();
+      var gain = audio.createGain();
+      gain.gain.setValueAtTime(0.1, audio.currentTime);
+
+      osc.connect(gain);
+      gain.connect(audio.destination);
+
+      osc.start(audio.currentTime);
+      osc.stop(audio.currentTime + 0.1);
+    }
+  }
+  function playRamp(e) {
+    e.preventDefault();
+
+    if (audio) {
+      var osc = audio.createOscillator();
+      var gain = audio.createGain();
+
+      gain.gain.setValueAtTime(0, audio.currentTime);
+      gain.gain.linearRampToValueAtTime(0.1, audio.currentTime + 0.01);
+
+      gain.gain.setValueAtTime(0.1, audio.currentTime + 0.99);
+      gain.gain.linearRampToValueAtTime(0, audio.currentTime + 1);
+
+      osc.connect(gain);
+      gain.connect(audio.destination);
+
+      osc.start(audio.currentTime);
+      osc.stop(audio.currentTime + 0.1);
+    }
+  }
+
+  document.getElementById('raw-osc').addEventListener('click', playRawOsc);
+  document.getElementById('sawtooth').addEventListener('click', playSawtooth);
+  document.getElementById('softer').addEventListener('click', playSofter);
+  document.getElementById('ramp').addEventListener('click', playRamp);
+
   var waveStep = 0;
 
   function step(t) {
@@ -6,17 +77,11 @@ ready(function() {
 
     waveforms.clearRect(0, 0, 1200, 800);
 
-    waveforms.fillStyle = '#333';
-    waveforms.font = '32px Share Tech Mono';
-    waveforms.textBaseline = 'top';
-
     waveforms.lineWidth = 6;
     waveforms.strokeStyle = '#333';
 
-    waveforms.fillText('Sine', 8, 8);
-
     waveforms.beginPath();
-    for (var i = 0, x = 0 - waveStep; i < 14; i++) {
+    for (var i = 0, x = 0 - waveStep; i < 11; i++) {
       waveforms.moveTo(x, 100);
       waveforms.quadraticCurveTo(x + 50, i % 2 == 0 ? 50 : 150, x + 100, 100);
       x += 100;
@@ -24,23 +89,19 @@ ready(function() {
     waveforms.stroke();
     waveforms.closePath();
 
-    waveforms.fillText('Triangle', 8, 208);
-
     waveforms.beginPath();
     waveforms.moveTo(-50 - waveStep, 250);
     waveforms.lineTo(50 - waveStep, 350);
-    for (var i = 0, x = 150 - waveStep; i < 14; i++) {
+    for (var i = 0, x = 150 - waveStep; i < 11; i++) {
       waveforms.lineTo(x, i % 2 == 0 ? 250 : 350);
       x += 100;
     }
     waveforms.stroke();
     waveforms.closePath();
 
-    waveforms.fillText('Sawtooth', 8, 408);
-
     waveforms.beginPath();
     waveforms.moveTo(-50 - waveStep, 550);
-    for (var i = 0, x = 150 - waveStep; i < 8; i++) {
+    for (var i = 0, x = 150 - waveStep; i < 6; i++) {
       waveforms.lineTo(x, 450);
       waveforms.lineTo(x, 550);
       x += 200;
@@ -48,17 +109,25 @@ ready(function() {
     waveforms.stroke();
     waveforms.closePath();
 
-    waveforms.fillText('Square', 8, 608);
-
     waveforms.beginPath();
     waveforms.moveTo(-50 - waveStep, 650);
-    for (var i = 0, x = 50 - waveStep; i < 15; i++) {
+    for (var i = 0, x = 50 - waveStep; i < 12; i++) {
       waveforms.lineTo(x, i % 2 == 0 ? 650 : 750);
       waveforms.lineTo(x, i % 2 == 0 ? 750 : 650);
       x += 100;
     }
     waveforms.stroke();
     waveforms.closePath();
+
+    waveforms.fillStyle = '#333';
+    waveforms.font = '64px Share Tech Mono';
+    waveforms.textBaseline = 'middle';
+
+    waveforms.clearRect(900, 0, 300, 800);
+    waveforms.fillText('Sine', 915, 100);
+    waveforms.fillText('Triangle', 915, 300);
+    waveforms.fillText('Sawtooth', 915, 500);
+    waveforms.fillText('Square', 915, 700);
 
     waveStep += 4;
     if (waveStep > 200) {
